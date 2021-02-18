671b74871c883df8af5b7ef83b10050a1372373a
DevShoesed <francesco.scarpato@gmail.com>
2021-02-18 07:07:55 +0100

    Now data are given from CSV files

--- index.php
+++ index.php
@@ -4,31 +4,36 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-use App\Model\Airport;
 use App\Model\Flight;
+use App\Model\Airport;
 use App\Repository\AirportRepository;
 use App\Repository\FlightRepository;
 use App\Solution\SolutionA;
 
+function csvToAirport($row): Airport
+{
+    $array = str_getcsv($row);
+    return new Airport((int)$array[0], $array[1], $array[2], (float)$array[3], (float)$array[4]);
+}
+
+
+function csvToFlight($row): Flight
+{
+    $array = str_getcsv($row);
+    return new Flight($array[0], $array[1], (float)$array[2]);
+}
+
+
 /* Set Airport Repository */
 
 $airportTable = new AirportRepository();
-$airportTable->setData([
-    new Airport(1, "Roma Fiumicino", "FCO", 0, 0),
-    new Airport(2, "Milano Malpensa", "MXP", 0, 0),
-    new Airport(3, "Napoli", "NAP", 0, 0),
-    new Airport(4, "Venezia", "VCE", 0, 0),
-    new Airport(5, "Palermo", "PMO", 0, 0),
-]);
+$airportTable->setData(array_map("csvToAirport", file("./data/airports.csv")));
 
 /* Set Flights Repository */
 $flightTable = new FlightRepository();
-$flightTable->setData([
-    new Flight("NAP", "FCO", 100),
-    new Flight("FCO", "PMO", 150),
-    new Flight("PMO", "MXP", 200),
-    new Flight("NAP", "MXP", 500)
-]);
+$flightTable->setData(array_map("csvToFlight", file("./data/flights.csv")));
+
 
 /* Instance of Solution for find best price */
 $solution = new SolutionA($airportTable, $flightTable);
+echo $solution->getBestPrice("NAP", "MXP", 2)["price"];
